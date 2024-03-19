400caf14b88eacb6d214f94c38ff8c381efbf0b5
CunGemcrak <142614397+CunGemcrak@users.noreply.github.com>
2024-03-19 10:22:16 -0500

    Actualizado hasta agregar Drop y Sql

--- Proyecto_Final_PooCUN/Script/Script.js
+++ Proyecto_Final_PooCUN/Script/Script.js
@@ -1,113 +1,71 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Botones a activar
     const modeloInput = document.getElementById('Modelo');
-    const mensajeError = document.getElementById('Modelo-error');
+    const placaInput = document.getElementById('Placa');
+    const MarcaaInput = document.getElementById('Marca');
+    const ColorInput = document.getElementById('Color');
+    const limpiarBtn = document.getElementById('btnLimpiar');
+    const CreaBDInput = document.getElementById('CreaBD');
+    const textarea = document.getElementById('CreaTabla');
 
     modeloInput.addEventListener('input', () => {
+        const mensajeError = document.getElementById('Modelo-error');
         let modeloValue = modeloInput.value.trim();
-        // Eliminar caracteres que no sean números del valor ingresado
         modeloValue = modeloValue.replace(/\D/g, '');
-
-        // Limitar el valor a 4 caracteres
         modeloValue = modeloValue.slice(0, 4);
-
-        // Actualizar el valor del input con solo 4 números
         modeloInput.value = modeloValue;
-
-        // Verificar si el valor ingresado contiene exactamente 4 dígitos
-        if (modeloValue.length === 4 ) {
-            mensajeError.textContent = ''; // Borrar mensaje de error
+        if (modeloValue.length === 4) {
+            mensajeError.textContent = '';
         } else {
             mensajeError.textContent = 'Ingresa solo 4 números.';
         }
     });
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const placaInput = document.getElementById('Placa');
-    const mensajeError = document.getElementById('Placa-error');
 
     placaInput.addEventListener('input', () => {
+        const mensajeError = document.getElementById('Placa-error');
         placaInput.value = placaInput.value.toUpperCase();
-
         const placaValue = placaInput.value.trim().toUpperCase();
         const regex = /^[A-Z]{3}\d{3}$|^[A-Z]{3}\d{2}[A-Z]$/;
-
         if (regex.test(placaValue)) {
             mensajeError.textContent = '';
         } else {
             mensajeError.textContent = 'Placa inválida.';
         }
-
-
     });
-});
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const placaInput = document.getElementById('CreaBD');
-    const mensajeError = document.getElementById('CreaBD-error');
-
-    placaInput.addEventListener('input', () => {
-        let inputValue = placaInput.value.trim().toLowerCase();
+    CreaBDInput.addEventListener('input', () => {
+        const mensajeError = document.getElementById('CreaBD-error');
+        let inputValue = CreaBDInput.value.trim().toLowerCase();
         inputValue = inputValue.replace(/[0-9]/g, ''); // Eliminar caracteres que sean números
-
-        placaInput.value = inputValue; // Asignar el nuevo valor al campo
-
+        CreaBDInput.value = inputValue; // Asignar el nuevo valor al campo
         if (inputValue === '') {
             mensajeError.textContent = 'El campo no puede estar vacio.';
         } else {
             mensajeError.textContent = '';
         }
     });
-});
 
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const placaInput = document.getElementById('Marca');
-    const mensajeError = document.getElementById('Marca-error');
-
-    placaInput.addEventListener('input', () => {
-        placaInput.value = placaInput.value.toUpperCase();
-        if (placaInput.value.length > 15) {
-            placaInput.value = placaInput.value.slice(0, 15);
-            mensajeError = 'maximo 15 caracteres';
+    MarcaaInput.addEventListener('input', () => {
+        const mensajeError = document.getElementById('Marca-error');
+        MarcaaInput.value = MarcaaInput.value.toUpperCase();
+        if (MarcaaInput.value.length > 15) {
+            MarcaaInput.value = MarcaaInput.value.slice(0, 15);
+            mensajeError.textContent = 'Máximo 15 caracteres';
+        } else {
+            mensajeError.textContent = '';
         }
-
-
     });
-});
 
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const placaInput = document.getElementById('Color');
-    const mensajeError = document.getElementById('Color-error');
-
-    placaInput.addEventListener('input', () => {
-        placaInput.value = placaInput.value.toUpperCase();
-        if (placaInput.value.length > 10) {
-            placaInput.value = placaInput.value.slice(0, 15);
-            mensajeError = 'maximo 10 caracteres';
+    ColorInput.addEventListener('input', () => {
+        const mensajeError = document.getElementById('Color-error');
+        ColorInput.value = ColorInput.value.toUpperCase();
+        if (ColorInput.value.length > 10) {
+            ColorInput.value = ColorInput.value.slice(0, 10);
+            mensajeError.textContent = 'Máximo 10 caracteres';
+        } else {
+            mensajeError.textContent = '';
         }
-
-
     });
-});
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const limpiarBtn = document.getElementById('btnLimpiar');
 
     limpiarBtn.addEventListener('click', () => {
         document.getElementById('Placa').value = '';
@@ -119,9 +77,8 @@ document.addEventListener('DOMContentLoaded', () => {
         document.getElementById('Modelo-error').textContent = '';
         document.getElementById('Color-error').textContent = '';
     });
+
+    textarea.addEventListener('input', () => {
+        textarea.value = textarea.value.toLowerCase();
+    });
 });
-
-
-
-
-
--- Proyecto_Final_PooCUN/Script/Scriptvalidation.js
+++ Proyecto_Final_PooCUN/Script/Scriptvalidation.js
@@ -24,31 +24,191 @@ function CrearDB(botonId) {
             .then(response => response.text())
             .then(data =>{
                 mensajeError.innerText = data;
-             //   alert(data);
+                verDB('mensaje');
+
             })
             .catch(error => { 
-              //  mensajeError.innerText = 'Error:'. error;
-             //   alert('Error:' + error);
-                console.error('Error:' + error);
+                             console.error('Error:' + error);
             });
-          //  document.getElementById('miFormulario').submit();
+         
         }
+       
     }
-  //  return validarFormulario();
+
+
+   
 }
 
 
 
-function EliminarDB(botonId){
-    let creaBDValue = document.getElementById('CreaBD').value.trim();
-    // alert("el valor enviado" + creaBDValue);
-     let mensajeError = document.getElementById('CreaBD-error');
-    if (botonId === 'btnEliminarDB') {
-    if(creaBDValue === ''){
-        mensajeError.innerText = 'Campo vacio';
+
+
+function verDB(mensaje) {
+    mensaje;
+   // alert("Entro en ver BD")
+    const listaBasesDatos = document.getElementById('listaBasesDatos');
+    const miSelectbd = document.getElementById('miSelectbd');
+    miSelectbd.innerHTML = '';
+    const optionDefault = document.createElement('option');
+    optionDefault.value = "no";
+    optionDefault.textContent = "Selecciona BD";
+    miSelectbd.appendChild(optionDefault);
+
+    
+    fetch('php/phpauxiliar/busqueda.php')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Hubo un problema al obtener los datos.');
+            }
+            return response.json(); // Obtener la respuesta como JSON
+        })
+        .then(data => {
+            console.log(data); // Mostrar la respuesta en la consola
+            // Verificar si data.basesDatos es un array antes de usar forEach
+            
+            if (Array.isArray(data.basesDatos)) {
+                // Limpiar contenido previo
+                listaBasesDatos.innerHTML = '';
+
+                // Crear la tabla
+                const table = document.createElement('table');
+                const thead = document.createElement('thead');
+                const tbody = document.createElement('tbody');
+
+                // Crear la fila de encabezado
+                const headerRow = document.createElement('tr');
+                const nameHeader = document.createElement('th');
+                nameHeader.textContent = 'Bases de Datos';
+                headerRow.appendChild(nameHeader);
+                thead.appendChild(headerRow);
+                table.appendChild(thead);
+
+                // Agregar filas para cada base de datos
+                data.basesDatos.forEach(baseDatos => {
+                    const row = document.createElement('tr');
+                    const nameCell = document.createElement('td');
+                    nameCell.textContent = baseDatos;
+                    row.appendChild(nameCell);
+
+                    // Crear el botón de eliminar
+                    const deleteButton = document.createElement('div');
+                    deleteButton.textContent = 'Eliminar';
+                    deleteButton.name = baseDatos;
+                    deleteButton.id = baseDatos; 
+                    deleteButton.classList.add('btnEliminar');
+                   // deleteButton.dataset.baseDatos = baseDatos; // Guardar el nombre de la base de datos
+                   // deleteButton.onclick(eliminarBaseDatos(baseDatos));
+                    deleteButton.addEventListener('click', eliminarBaseDatos);
+                    const buttonCell = document.createElement('td');
+                    buttonCell.appendChild(deleteButton);
+                    row.appendChild(buttonCell);
+
+                    tbody.appendChild(row);
+
+                    // Agregar opción al select
+                    const option = document.createElement('option');
+                    option.value = baseDatos;
+                    option.textContent = baseDatos;
+                    miSelectbd.appendChild(option);
+
+                });
+
+                table.appendChild(tbody);
+                listaBasesDatos.appendChild(table);
+            } else {
+                console.error('Los datos obtenidos no son un array.');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function eliminarBaseDatos(event) {
+    alert(event.target.name);
+    let bd = event.target.name;
+
+    const formData = new FormData();
+    formData.append('bd', bd); 
+    
+
+    fetch('php/Eliminar.php', {
+        method: 'POST',
+        body:formData, // Convertir el valor a JSON
+      
+    })
+    
+    .then(response => response.text())
+    .then(data =>{
+        if (data.trim() === "Bd Borrada correctamente" ||  data.trim() === "Error al borrar crear la bd") {
+       //     mensajeError.innerText = data.trim();
+            alert(data.trim());
+        } 
+                   
+        else {
+           // mensajeError.innerText = 'Error al borrar BD';
+            alert('Error al borrar BD');
+        }
+        console.error(data);
+       // alert(data);  // Muestra la respuesta del servidor en un alert
+        verDB('mensaje');
+       
+
+    })
+    .catch(error => { 
+                     console.error('Error:' + error);
+    });
+
+
+   
+
+    
+}
+
+
+
+function btnCrearTable(mensaje){
+    mensaje; 
+
+    const valueBD = document.getElementById('miSelectbd').value;
+    const tablaBD = document.getElementById('CreaTabla').value;
+    const mensajeError = document.getElementById('CreaTabla-error');
+   // alert("miSelectbd"+valueBD);
+ //   alert("CreaTabla"+tablaBD);
+
+    if(valueBD ==="no" ){
+        mensajeError.innerText = "Selecciona una base de datos correcta";
+    }else
+    if(tablaBD ===""){
+        mensajeError.innerText = "Agrega datos de SQL";
     }else{
-        mensajeError.innerText = 'Campo lleno';
+        const formData = new FormData();
+        formData.append('bd', valueBD); // Agregar el valor de miSelectbd al FormData
+        formData.append('tabla', tablaBD); 
+
+
+            fetch('php/CrearTablas.php', {
+                method: 'POST',
+                body: formData, // enviamos los datos 
+              
+            })
+            
+            .then(response => response.text())
+            .then(data =>{
+                if (data.trim() === "Tabla Creada correctamente" ||  data.trim() === "Error al crear la Tabla") {
+                    mensajeError.innerText = data.trim();
+                } 
+                           
+                else {
+                    mensajeError.innerText = 'Error al crear la tabla';
+                }
+                console.error(data);
+                alert(data);  // Muestra la respuesta del servidor en un alert
+               
+
+            })
+            .catch(error => { 
+                            console.error('Error:' + error);
+            });
     }
-    }
+
 }
 
